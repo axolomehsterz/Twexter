57a180894bf8a1ce9ec0f2b64bbf00f397c76434
Alex Vranas <avranas42@gmail.com>
2024-02-27 21:55:53 -0800

    Add post twexta

--- spring-server/src/main/java/com/teamAxolomeh/twexter/TwextController.java
+++ spring-server/src/main/java/com/teamAxolomeh/twexter/TwextController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,23 +29,63 @@ public class TwextController {
     this.databaseQueryExecutor = databaseQueryExecutor;
   }
 
+  class GetTwextaResponse {
+    private List<Map<String, Object>> result;
+
+    GetTwextaResponse(List<Map<String, Object>> content) {
+      this.result = content;
+    }
+    public List<Map<String, Object>> getResult() {
+      return this.result;
+    }
+  }
+
+  class PostTwextaResponse {
+    private String result;
+
+    PostTwextaResponse(String content) {
+      this.result = content;
+    }
+    public String getResult() {
+      return this.result;
+    }
+  }
+
   @GetMapping
-  public ResponseEntity<?> getAllTwexts(HttpServletRequest req, HttpServletResponse res) {
+  public ResponseEntity<GetTwextaResponse> getAllTwexts(HttpServletRequest req, HttpServletResponse res) {
     try {
-      final String query = "SELECT u.*, t.content FROM users u JOIN twexta t ON t.user_id=?;";
       Claims user = (Claims) req.getAttribute("user");
+      final String query = "SELECT u.*, t.content FROM users u JOIN twexta t ON t.user_id=?;";
       Object[] params = new Object[] { user.get("_id") };
       List<Map<String, Object>> results = databaseQueryExecutor.query(query, params);
-      return ResponseEntity.ok().body(results);
+      GetTwextaResponse response = new GetTwextaResponse(results);
+      return ResponseEntity.ok().body(response);
     } catch (Exception e) {
       logger.info("Error!");
       logger.error(e.getLocalizedMessage());
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+      GetTwextaResponse response = new GetTwextaResponse(null);
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
-  } 
+  }
 
-  // @PostMapping("/")
-  // public ResponseEntity<String> login(HttpServletResponse res, @RequestBody LoginDto data) {
-
-  // }
+  @PostMapping
+  public ResponseEntity<PostTwextaResponse> login(HttpServletRequest req, HttpServletResponse res, @RequestBody PostTwextaDto data) {
+    try {
+      Claims user = (Claims) req.getAttribute("user");
+      final String query = "INSERT INTO twexta (content, user_id) VALUES (?, ?) RETURNING *;";
+      System.out.println(user.get("_id"));
+      Object[] params = new Object[] { data.getTwextContent(), user.get("_id") };
+      List<Map<String, Object>> results = databaseQueryExecutor.query(query, params);
+      final String message = (results != null) ? "Stored the twext" : "DID NOT store the twext";
+      PostTwextaResponse response = new PostTwextaResponse(message);
+      System.out.println(response);
+      return ResponseEntity.ok().body(response);
+    }
+      catch (Exception e) {
+      logger.info("Error!");
+      logger.error(e.getLocalizedMessage());
+      PostTwextaResponse response = new PostTwextaResponse(e.getMessage());
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+  }
 }
